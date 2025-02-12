b11df78fd5e4504c2ea1fa354e306a77815834c1
kindness <130109724+pdrozxt@users.noreply.github.com>
2025-02-12 06:55:20 +0000

    Exercícios concluido em todas as dificuldades

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main () {
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Declaração das variáveis das cidades
+    int pontos_turisticos, pontos_turisticos2;
+    double area_cidade, populacao_cidade, area_cidade2, populacao_cidade2;
+    double super_poder, super_poder2;
+    double densidade_pop, PIB_percap, PIB_cidade;
+    double densidade_pop2, PIB_percap2, PIB_cidade2;
+
+
+    // Dados da primeira cidade inseridos pelo usuário e armazenado na memória
+    printf("Digite a populacao da primeira cidade: ");
+    scanf("%lf", &populacao_cidade);
+    printf("Digite a area da primeira cidade: ");
+    scanf("%lf", &area_cidade);
+    printf("Digite o PIB da primeira cidade: ");
+    scanf("%lf", &PIB_cidade);
+    printf("Digite o numero de pontos turisticos da primeira cidade: ");
+    scanf("%d", &pontos_turisticos);
+
+    // Atribuição das funcionalidades de densidade pop. e pib per capita da primeira cidade
+    densidade_pop = (populacao_cidade / area_cidade);
+    PIB_percap = (PIB_cidade / populacao_cidade);
+
+    // Impressão de todos os dados da primeira cidade
+    printf("\n---- DADOS DA PRIMEIRA CIDADE ----\n");
+    printf("Populacao: %.2lf\n", populacao_cidade);
+    printf("Area: %.2lf\n", area_cidade);
+    printf("PIB: %.2lf\n", PIB_cidade);
+    printf("Pontos Turisticos: %d\n", pontos_turisticos);
+    printf("PIB per Capita: %.2lf\n", PIB_percap);
+    printf("Densidade Populacional: %.2lf\n\n", densidade_pop);
+
+    // Dados da segunda cidade inseridos pelo usuário e armazenado na memória
+    printf("Digite a populacao da segunda cidade: ");
+    scanf("%lf", &populacao_cidade2);
+    printf("Digite a area da segunda cidade: ");
+    scanf("%lf", &area_cidade2);
+    printf("Digite o PIB da segunda cidade: ");
+    scanf("%lf", &PIB_cidade2);
+    printf("Digite o numero de pontos turisticos da segunda cidade: ");
+    scanf("%d", &pontos_turisticos2);
+
+    // Atribuição das funcionalidades de densidade pop. e pib per capita da segunda cidade
+    densidade_pop2 = (populacao_cidade2 / area_cidade2);
+    PIB_percap2 = (PIB_cidade2 / populacao_cidade2);
+
+    // Impressão de todos os dados da segunda cidade
+    printf("\n---- DADOS DA SEGUNDA CIDADE ----\n");
+    printf("Populacao: %.2lf\n", populacao_cidade2);
+    printf("Area: %.2lf\n", area_cidade2);
+    printf("PIB: %.2lf\n", PIB_cidade2);
+    printf("Pontos Turisticos: %d\n", pontos_turisticos2);
+    printf("PIB per Capita: %.2lf\n", PIB_percap2);
+    printf("Densidade Populacional: %.2lf\n\n", densidade_pop2);
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Atribuição da funcionalidade do super poder
+    super_poder = (populacao_cidade + area_cidade + PIB_cidade + pontos_turisticos);
+    super_poder2 = (populacao_cidade2 + area_cidade2 + PIB_cidade2 + pontos_turisticos2);
 
+    if (densidade_pop < densidade_pop2)
+    {
+        printf("A primeira cidade e a vencedora no quesito densidade populacional com %lf pontos!\n", densidade_pop);
+    } else { 
+        printf ("A segunda cidade e a vencedora no quesito densidade populacional com %lf pontos!\n", densidade_pop2);
+    }
+    if (super_poder > super_poder2)
+    {
+        printf("A primeira cidade e a vencedora no quesito super poder com %.2lf pontos!\n", super_poder);
+    } else { 
+        printf("A segunda cidade e a vencedora no quesito super poder com %.2lf pontos!\n", super_poder2);
+    }
+
+    system ("pause");
     return 0;
 }
